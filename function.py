9b39e62a0c5ed5eec6fb9632336223e3110029ee
Neetesh Prajapati <139002314+Neetesh26@users.noreply.github.com>
2024-12-11 18:12:59 +0530

    function added

--- function.py
+++ function.py
@@ -3,3 +3,17 @@ def calc_avg(a,b,c):
     return avg
 
 print(calc_avg(98,97,95))
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+def my_function(fname):
+  print(fname + " neetesh")
+
+my_function("school_name")
+my_function("college")
+my_function("home_name")
